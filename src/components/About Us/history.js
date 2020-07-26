1f12f8a3fd58f10faafad4f065071f7b07a0503b
AmmarKhalid123 <ammarkhalid237@gmail.com>
2020-07-27 04:55:20 +0500

    History Updated@

--- src/components/About Us/history.js
+++ src/components/About Us/history.js
@@ -1,27 +1,16 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Container, Row, Col } from 'reactstrap';
-
-
-
 import FontAwesome from 'react-fontawesome';
 import faStyles from 'font-awesome/css/font-awesome.css'
 
-
-
-
 function History(){
-
-    
-    
     return (
           <React.Fragment>
               <Container fluid={true} style = {{textAlign: "center"}}>
                     <Row>
-                
                         <Col  md={{size: 6}} sm = {{size: 12}} style = {{backgroundColor: "#e50000"}}>
-                        
-                         <p className = "our-hist-txt">OUR HISTORY</p>
+                         <p className = "our-hist-txt">OUR <br/> HISTORY</p>
                         </Col> 
 
                         <Col  md={{size: 6}} sm = {{size: 12}}>
@@ -34,11 +23,109 @@ function History(){
                          <FontAwesome className = 'fa-quote-right'
                                     name = 'quote-right'
                                     size = '3x' style = {{color:"#e50000", float: 'right'}}/>
-                        <p className = 'text-left hist-author'> <br/> <br/>Margaret Mead</p>
+                        <p className = 'text-left hist-author'> <br/> <br/> - Margaret Mead</p>
                         </Col> 
                 
                     </Row>
             </Container>
+            <Container className="hist-main mt-3">
+                <p className="col-12 mb-3" style={{textAlign: "justify"}}>
+                Three students of Dow Medical College, moved by the despair and needs of people entering the doors of Dr. Ruth K.M. Pfau Civil Hospital, then known simply as Civil Hospital Karachi for treatment of their ailments, laid down the foundation of Patients’ Welfare Association in 1979. PWA aims to provide services not offered by Civil Hospital Karachi thus when the hospital began to offer services we catered to we stopped offering them and concentrated our energies on other needs of the patients. The following is an account of our work.
+                </p>
+                <div  style={{width: '100%', textAlign: 'center', color: '#000', marginBottom: '3px'}}>
+                    <h1 className = 'display-6 mt-5 mb-2'>DRUG BANK</h1>
+                    <img src={require('../../images/divider.png')}/>
+                </div>
+                <p className="col-12 mb-3">
+                    PWA’s Drugbank, provided for the basic medicinal needs of patients admitted in Dr. Ruth K.M. Pfau Civil Hospital for almost 4 decades, however it is no longer functional because Dr Ruth Pfau Civil Hospital Karachi now offers free medicines to patients.
+                </p>
+                <div style={{width: '100%', textAlign: 'center', color: '#000', marginBottom: '3px'}}>
+                    <h1 className = 'display-6 mt-5 mb-2'>BLOOD BANK</h1>
+                    <img src={require('../../images/divider.png')}/>
+                </div>
+                <p className="col-12 mb-3">
+                    PWA Blood Bank provides services of quality assured, screened blood products such as packed cells and fresh frozen plasma. It is being recently upgraded to the status of one of 3 regional blood centers planned by the government to fulfill the blood related needs of Karachi.
+                </p>
+                <div style={{width: '100%', textAlign: 'center', color: '#000', marginBottom: '3px'}}>
+                    <h1 className = 'display-6 mt-5 mb-2'>TB FOLLOWUP CLINIC</h1>
+                    <img src={require('../../images/divider.png')}/>
+                </div>
+                <p className="col-12 mb-3">
+                PWA TB Follow-up Clinic was started in 1984, shortly followed by its General Follow-up Clinic. These departments catered to patients suffering from long term ailments. TB follow-up annually catered to more than 500 patients suffering from tuberculosis infections. This too is no longer functional because this service is now offered by the hospital.
+                </p>
+                <div style={{width: '100%', textAlign: 'center', color: '#000', marginBottom: '3px'}}>
+                    <h1 className = 'display-6 mt-5 mb-2'>DIAGNOSTIC LAB</h1>
+                    <img className="mx-auto" src={require('../../images/divider.png')}/>
+                </div>
+                <p className="col-12 mb-3">
+                PWA Diagnostic Lab was started in 1986 to provide baseline hematological and biochemical tests to the patients free of cost. The laboratory performed complete blood count (CBC) with peripheral film study amongst many other important investigative tests. Our diagnostic lab no longer offers these tests to the public.
+                </p>
+                <div style={{width: '100%', textAlign: 'center', color: '#000', marginBottom: '3px'}}>
+                    <h1 className = 'display-6 mt-5 mb-2'>THALESSEMIA SERVICES</h1>
+                    <img className="mx-auto" src={require('../../images/divider.png')}/>
+                </div>
+                <p className="col-12 mb-3">
+                    PWA Thalassemia Services started in 2011, now better known as Thalassemia Daycare Center holds the promise of a long and prosperous life for the poor innocent children, diagnosed with the blood disorder thalassemia at birth. Many of these individuals have found themselves unable to handle the financial burden of the lifelong treatment of this ailment which PWA is currently providing to 263 registered patients, all free of cost.
+                </p>
+    
+            </Container>
+            <Container fluid={true} style = {{textAlign: "center"}}>
+                    <Row>
+                        <Col  md={{size: 6}} sm = {{size: 12}} style = {{backgroundColor: "#e50000"}}>
+                         <p className = "our-hist-txt">WHO ARE WE?</p>
+                        </Col> 
+
+                        <Col  md={{size: 6}} sm = {{size: 12}}>
+                            <div>
+                        <FontAwesome className = 'fa-quote-left'
+                                    name = 'quote-left'
+                                    size = '3x' style = {{color:"#e50000", float: 'left', marginTop: '15px'}}/>
+                            </div>   
+                         <p className = 'hist-quote'> <br/> <br/> <br/>PWA is a story of love and respect for life, fueled by compassion and sincere desire for its preservation.</p>
+                         <FontAwesome className = 'fa-quote-right'
+                                    name = 'quote-right'
+                                    size = '3x' style = {{color:"#e50000", float: 'right'}}/>
+                        </Col> 
+                
+                    </Row>
+            </Container>
+            <Container fluid={true} style = {{textAlign: "center", marginTop: '10px'}}>
+                    <Row>
+                        <Col  md={{size: 6}} sm = {{size: 12}} style = {{backgroundColor: "#e50000"}}>
+                         <p className = "our-hist-txt">OUR IMPACT</p>
+                        </Col> 
+
+                        <Col  md={{size: 6}} sm = {{size: 12}}>
+                            <div>
+                        <FontAwesome className = 'fa-quote-left'
+                                    name = 'quote-left'
+                                    size = '3x' style = {{color:"#e50000", float: 'left', marginTop: '15px'}}/>
+                            </div>   
+                         <p className = 'hist-quote'> <br/> <br/> <br/>Patients are our concern, their health our priority &amp; serving humanity, our motto.</p>
+                         <FontAwesome className = 'fa-quote-right'
+                                    name = 'quote-right'
+                                    size = '3x' style = {{color:"#e50000", float: 'right'}}/>
+                        </Col> 
+                    </Row>
+            </Container>
+            <Container className="mt-3">
+                <Row>
+                    <Col xs='1'>
+                    </Col>
+                    <Col xs='10' className="hist-main" style={{lineHeight: 1.8}}>
+                        <p >For nearly 4 decades, Patients’ Welfare Association has striven to serve the destitute entering Dr. Ruth K.M. Pfau Civil Hospital with dedication and professionalism. All the data shown below is a living testament to that fact.</p>
+                        <ul>
+                            <li>Our blood bank has provided 1.4 million blood products to the masses.</li>
+                            <li>PWA Drugbank has disbursed medicines and injections worth over PKR 92 million to the needy, benefiting roughly 8000-10,000 patients each year, all free of cost.</li>
+                            <li>PWA Thalassemia Daycare Center has spent over PKR 52 million on treatments such iron chelation therapy. This costs stands apart from the cost of providing regular screened blood transfusions which often exceed 7500 in number each year.</li>
+                            <li>PWA has spent over PKR 704 million on the welfare of others and with the continued support of our kind donors and well-wishers, we hope to serve a lot more.</li>
+                        </ul>
+                    </Col>
+                    <Col xs='1'>
+                    </Col>
+                </Row>
+                
+            </Container>            
           </React.Fragment>
 
         );
