824a9d3b94aaf4bcfb171c8ac2f6ae7d5d50c282
Ammar Khalid <ammarkhalid237@gmail.com>
2020-08-19 00:09:29 +0500

    Calculator Convertor In Progress

--- src/App.js
+++ src/App.js
@@ -26,13 +26,15 @@ import Blogs from './components/NewsAndStories/blogs';
 import ImpDonor from './components/blogs-pages/important-donor';
 import ThalessemiaQuiz from './components/blogs-pages/thalessemia-quiz';
 import Events from './components/NewsAndStories/events';
+import ZakaatCalculator from './components/Services/calculateZakaat';
+import Converter from './components/Services/currencyConverter';
 
 
 function App() {
   return (
     <div>
       <Header/>
-      <Events/>
+      <Converter/>
       <Footer/>
     </div>
   );
---
+++ src/components/Services/currencyConverter.js
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+
+export default function Converter () {
+    const [currencies, setCurrencies] = useState('');
+    const [amount, setAmt] = useState('');
+    const [fromCurrency, setFromCur] = useState('USD');
+    const [toCurrency, setToCur] = useState('PKR');
+    const [result, setResult] = useState('');
+
+    useEffect(() => {
+        axios.get('https://free.currconv.com/api/v7/currencies?apiKey=0b15c491fe7a9edb357d')
+        .then((res) => {
+            var a = [];
+            for (const key in res.data.results){
+                a.push(key);
+            }
+            setCurrencies(a);
+        })
+        .catch(err => console.log(err))
+    }, []);
+
+    const selectHandler = event => {
+          if (event.target.name === "from") {
+            setFromCur(event.target.value);
+          } else {
+            if (event.target.name === "to") {
+              setToCur(event.target.value);
+            }
+          }
+        };
+    const convertHandler = () => {
+            if (fromCurrency !== toCurrency) {
+                axios
+                .get(
+                    `https://free.currconv.com/api/v7/convert?q=${fromCurrency}_${toCurrency}&compact=ultra&apiKey=0b15c491fe7a9edb357d`
+                )
+                .then(response => {
+                    console.log(response);
+                    const conv = `${fromCurrency}_${toCurrency}`;
+                    const ans = response.data[conv] * amount;
+                    setResult(ans);
+                })
+                .catch(error => {
+                console.log("Opps", error);
+                });
+            } else {
+              setResult("You cant convert the same currency!");
+            }
+        };
+    return(
+        <div className="Converter">
+          <h2>
+            <span>Currency</span>Converter
+          </h2>
+          <div className="From">
+            <input
+              name="amount"
+              type="text"
+              value={amount}
+              onChange={event => setAmt(event.target.value)}
+            />
+            <select
+              name="from"
+              onChange={event => selectHandler(event)}
+              value={fromCurrency}
+            >
+              {currencies && currencies.map(cur => (
+                <option key={cur}>{cur}</option>
+              ))}
+            </select>
+            <select
+              name="to"
+              onChange={event => selectHandler(event)}
+              value={toCurrency}
+            >
+              {currencies && currencies.map(cur => (
+                <option key={cur}>{cur}</option>
+              ))}
+            </select>
+            <button onClick={convertHandler}>Convert</button>
+            {result && <h3>{result}</h3>}
+          </div>
+        </div>
+    )
+}
